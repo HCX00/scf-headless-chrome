d6ca458fcb687c36b99a38f37efae45ab52b22a6
HCX00 <40118942+HCX00@users.noreply.github.com>
2020-07-05 19:23:01 +0800

    Update index.py
    auto copy  chromium&chromedriver to /tmp and chmod +x

--- index.py
+++ index.py
@@ -1,63 +1,25 @@
 # -*- coding: utf8 -*-
-import json
+import os
+import stat
+import shutil
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
 
 
-class Web:
-    def __init__(self, options, path='./chromedriver/chromedriver', screen_shot_path='./'):
-        self._driver = webdriver.Chrome(path, chrome_options=options)
-        self.screen_shot_count = 0
-        self.screen_shot_path = screen_shot_path
+def main_handler(event, context):
+    # 复制文件到 /tmp 并赋执行权限
+    shutil.copytree('./chromedriver', '/tmp/chromedriver')
+    files = ['/tmp/chromedriver/' + filename for filename in os.listdir('/tmp/chromedriver/')]
+    for filepath in files:
+        os.chmod(filepath, os.stat(filepath).st_mode | stat.S_IXUSR)
 
-    def get_screen_shot(self, element_id=None):
-        if element_id is not None:
-            ActionChains(self._driver).move_to_element(self._driver.find_element_by_id(element_id)).perform()
-            self._driver.save_screenshot('{}/{}.png'.format(self.screen_shot_path, self.screen_shot_count))
-        else:
-            self._driver.save_screenshot('{}/{}.png'.format(self.screen_shot_path, self.screen_shot_count))
-        print('screenshot {} saved!'.format(self.screen_shot_count))
-        self.screen_shot_count += 1
-        return self
-
-    def save_page_source(self):
-        with open("{}.html".format(self._driver.title), 'w') as f:
-            f.write(self._driver.page_source)
-        return self
-
-    def get_page_source(self):
-        return self._driver.page_source
-
-    def find_element_by_id(self, id_):
-        return self._driver.find_element_by_id(id_)
-
-    def find_element_by_xpath(self, xpath):
-        return self._driver.find_element_by_xpath(xpath)
-
-    def print_page(self):
-        print(self._driver.page_source)
-
-    def go_to(self, url):
-        self._driver.get(url)
-        return self
-
-
-def main():
+    # 设置 chrome
     options = Options()
-    options.binary_location = './chromedriver/headless-chromium-tencent'
+    options.binary_location = '/tmp/chromedriver/headless-chromium-tencent'
     options.add_argument("--headless")
     options.add_argument('--no-sandbox')
     options.add_argument('--single-process')
     options.add_argument('--disable-dev-shm-usage')
     options.add_experimental_option('excludeSwitches', ['enable-automation'])
     options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
-    web = Web(options)
-    return json.loads(web.go_to('https://postman-echo.com/get').find_element_by_xpath('//body').text)
-
-def test():
-    return platform.platform()
-
-def main_handler(event, context):
-    return main()
-
+    browser = webdriver.Chrome('/tmp/chromedriver/chromedriver', chrome_options=options)
